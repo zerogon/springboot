5a3f3be8ffec1669ef4a945bcf96d53851e75c08
zerogon <safoma@naver.com>
2022-08-29 22:59:17 +0900

    buildspec.yaml add

--- src/test/java/zergon/grp/papercpnPro/PapercpnProApplicationTests.java
+++ src/test/java/zergon/grp/papercpnPro/PapercpnProApplicationTests.java
@@ -2,12 +2,12 @@ package zergon.grp.papercpnPro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
 @SpringBootTest
 class PapercpnProApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
 	}
 
-}
+}*/
